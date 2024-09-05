dff2b1ccb1648e3efc548855599360732f34f9c5
Me <Me>
2024-09-05 15:30:53 +0200

    Script for creating tablespace added

---
+++ create_tablespace.sql
@@ -0,0 +1,17 @@
+-- Create tablespace
+create tablespace railway_data
+    datafile '/opt/oracle/oradata/pdb_railway/railway_data.dbf'
+    size 500M autoextend on next 100M maxsize unlimited
+    logging
+    extent management local
+    autoallocate
+    segment space management auto;
+
+-- Create data owner
+create user data_owner identified by Owner56
+    default tablespace railway_data
+    quota unlimited on railway_data;
+
+-- Grant privilages to data owner
+grant connect, resources to data_owner;
+grant create table, create view, create procedure, create sequence, create index to data_owner;
